4c0e427e1ebd507c38f8c5387c7f730ba212735e
study-hary-id <haryansyah.study.id@gmail.com>
2022-04-29 14:40:56 +0700

    Update new-settings template

--- templates/new-settings.php
+++ templates/new-settings.php
@@ -1,6 +1,10 @@
 <div class="wrap">
     <h1>New Settings</h1>
     <form action="options.php" method="post">
-
+        <?php
+        settings_fields( 'settings_plugin_options' );
+        do_settings_sections( 'new_settings_plugin' );
+        submit_button();
+        ?>
     </form>
 </div>
